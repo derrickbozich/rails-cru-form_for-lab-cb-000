19f0cd3f17daef659312cfb40b0b2aec5559ec52
derrick b <dbozich1@gmail.com>
2018-08-14 21:06:25 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,6 +3,10 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
   end
 
+  def show
+    @artist = Artist.find_by_id(params[:id])
+  end
+
   def new
     @artist = Artist.new
   end
